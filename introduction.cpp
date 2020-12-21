4462204985be043df0cfa9726082620e606a1f66
CristoferNava <cj.carmonanava@ugto.mx>
2020-12-21 10:58:08 -0600

    i

---
+++ introduction.cpp
@@ -0,0 +1,64 @@
+// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 sistema de base 10
+// operación: 1 + 1 = 2
+// +: operador, suma recibe dos operandos (binarios)
+// 2 + 3 = 5
+
+// operadores unarios
+// negación: not Verdadero = Falso
+
+// computar:> realizar cómputos: llegar a resultados numéricos
+
+// Computadores:
+//     CPU: Procesador: Hace cálculos
+//     RAM: Se encarga de recordar cosas a corto plazo
+//     Discos de almecenamiento: Hard Drive, SSD: Recuerdan a largo plazo
+
+// Programar: Darle a instrucciones a la computadora
+// Programación binario
+// 1 1 1 
+// 1 a
+// 1 1 b
+// 1 1 1 c
+// 1111 h 
+// 111111 o 
+// 11111111 l
+
+// 0 = false
+// 1 = true
+// h        o          l      a
+// 1111 0 111111 0 11111111 0 1 0
+
+// 10
+// 111111
+
+// Ensamablador
+// 2 + 2 = 4
+
+// Compiladores
+
+#include <stdio.h> // magia
+
+int main() { // magia
+    int number = 25; // Estamos usando memoria RAM
+    int number2 = 15;
+    // variable: 1) tipo=int. 2) id=number. 3) valor=25
+    int sum = number + number2; // la suma la hace el procesador (CPU)
+    // sum se guarda en la RAM
+
+    return 0; // mágico
+}
+
+
+// 42 [b, b, b, b,,,,,,,,,,,,,, bbbb,,,,,,,,,,,sum]
+//   number                     number2
+
+
+
+// 1 byte = 8 bits
+// RAM
+// 1 bit true, false, 1 byte 8 bits
+// [bytes, b, b, b, b, b, b, b, b]
+// [b, b, b, b, bm, bm, bm, bm]
+//        2      100000
+// [b, b, b, b]
+
